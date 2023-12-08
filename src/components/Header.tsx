d0b986147bf416078a4083a2a134987e220a8e8b
Louing00 <48049923+Louing00@users.noreply.github.com>
2023-12-08 17:10:32 +0800

    delelte vercel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,9 +62,6 @@ export default function Header() {
                 <span class="text-transparent font-extrabold bg-clip-text bg-gradient-to-r dark:from-yellow-300 from-yellow-600 dark:to-red-700 to-red-700 mr-1">
                   ChatPPT
                 </span>
-                <span class="ml-1 font-extrabold text-slate-7 dark:text-slate">
-                  Vercel
-                </span>
                 <a
                   class="ml-2 <sm:hidden"
                   href="https://github.com/ourongxing/chatgpt-vercel"
